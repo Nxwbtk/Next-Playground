85f9befb0c8e9e794dd309307b3cf1dee8cc32cc
NewDev <65420147@kmitl.ac.th>
2026-01-29 02:02:59 +0700

    Remove all

--- app/components/dashboard/schema.ts
+++
@@ -1,8 +0,0 @@
-import * as z from 'zod';
-export const schema = z.object({
-  total_documents: z.number(),
-  pending_documents: z.number(),
-  cancel_documents: z.number(),
-})
-
-export type IDashboard = z.infer<typeof schema>;
--- app/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-import million from 'million/compiler';
-const nextConfig = {
-	reactStrictMode: true,
-};
-
-// module.exports = nextConfig
-export default million.next(nextConfig);
